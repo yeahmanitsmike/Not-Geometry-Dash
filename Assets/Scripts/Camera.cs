f05f7f02a34d253134e4411070d27249770503b1
Dustin <dustin7212@gmail.com>
2020-03-08 18:17:59 -0500

    Create initial project

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    public Player player;
+    private Vector2 lastPlayerPosition;
+    private float distanceToMove;
+
+    void Start()
+    {
+        lastPlayerPosition = player.transform.position;
+    }
+
+    void Update()
+    {
+        distanceToMove = player.transform.position.x - lastPlayerPosition.x;
+        transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+        lastPlayerPosition = player.transform.position;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private new Collider2D collider;
+
+    public float moveSpeed = 100f;
+    public float jumpStrength = 150f;
+    public bool isGrounded;
+    public LayerMask groundLayer;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        rigidBody.velocity = new Vector2(moveSpeed, rigidBody.velocity.y);
+
+        isGrounded = Physics2D.IsTouchingLayers(collider, groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpStrength);
+        }
+
+    }
+}
