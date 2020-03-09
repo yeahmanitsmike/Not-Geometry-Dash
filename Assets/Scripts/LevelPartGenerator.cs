264a027cf19fb6c73e606559abfec17c64449234
Dustin <dustin7212@gmail.com>
2020-03-09 14:50:25 -0500

    Initial implementation of random level parts

--- Assets/Scripts/LevelPartGenerator.cs
+++ Assets/Scripts/LevelPartGenerator.cs
@@ -2,24 +2,22 @@
 
 public class LevelPartGenerator : MonoBehaviour
 {
-
-    public GameObject levelPart;
+    public GameObject[] levelParts;
     public Transform generationPoint;
-
-    private float levelPartWidth;
-
-    void Start()
-    {
-        levelPartWidth = levelPart.GetComponent<BoxCollider2D>().size.x;
-    }
+    
+    private float levelPartsWidth = 10f;
+    private int levelPartSelector;
 
     void Update()
     {
         if (transform.position.x < generationPoint.position.x)
         {
-            transform.position = new Vector3(transform.position.x + levelPartWidth, transform.position.y, transform.position.z);
 
-            Instantiate(levelPart, transform.position, transform.rotation);
+            levelPartSelector = Random.Range(0, levelParts.Length);
+
+            transform.position = new Vector3(transform.position.x + levelPartsWidth, transform.position.y, transform.position.z);
+
+            Instantiate(levelParts[levelPartSelector], transform.position, transform.rotation);
         }
     }
 }
