dd14e421811c16c339d54249c0480d73b40fdaee
Dustin <2127193+dustinn1@users.noreply.github.com>
2020-03-14 19:22:23 -0500

    Add score counter and create initial level design (#1)
    Co-authored-by: Jack Kotheimer <Jkotheimer@luc.edu>

---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+
+	public Text textBox;
+	public int score, iterations;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		textBox = GetComponent<Text>();
+		score = 0;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		iterations++;
+		if (iterations > 60) {
+			score++;
+			iterations = 0;
+		}
+		textBox.text = "Score: " + score.ToString();
+	}
+}
