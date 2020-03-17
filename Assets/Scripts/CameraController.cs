6d590956e604f6800e9d394c50ce4614628de3c7
yeahmanitsmike <59710638+yeahmanitsmike@users.noreply.github.com>
2020-03-17 17:19:21 -0500

    Camera tweak and slippy physics
    I added a few lines to the camera. Not really important lines. I also added slippy physics so that the character wont stick to the walls. But the player can still jump on the sides of the walls. Im trying to figure that out.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,16 +4,18 @@ public class CameraController : MonoBehaviour
 {
 
     public PlayerController player;
-    private Vector2 lastPlayerPosition;
+    private Vector3 lastPlayerPosition;
     private float distanceToMove;
 
     void Start()
     {
+        player = FindObjectOfType<PlayerController>();
         lastPlayerPosition = player.transform.position;
     }
 
     void Update()
     {
+
         distanceToMove = player.transform.position.x - lastPlayerPosition.x;
         transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
         lastPlayerPosition = player.transform.position;
